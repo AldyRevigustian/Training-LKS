fda196ed787eb975691144e49bda305cf2eca2fb
AldyRevigustian <aldyrevig@gmail.com>
2022-08-20 08:53:36 +0700

    20 Agustus

--- restaurant/src/App.vue
+++ restaurant/src/App.vue
@@ -1,31 +1,69 @@
 <script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import { computed } from '@vue/reactivity';
+import { reactive, ref } from 'vue';
+
+let count = ref(0);
+let name = ref("Aku");
+let isActive = ref(false)
+
+let person = reactive({
+  id : 1,
+  nama : 'aldy',
+  umur : 20
+})
+
+let doubleCount = computed(()=>{
+  return count.value * 2
+})
+
+
+let isTransactionSuccess = ref(0)
+
+let TransactionClass = computed(()=>{
+  return {'alert': true, 'alert-success':isTransactionSuccess.value == 1,'alert-danger': isTransactionSuccess.value==0 }
+})
+
+let isShow = ref(false)
+
+
+
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="lorem">
+    <!-- <p>Count : {{ count }}</p>
+    
+    <button v-on:click="count++">click me</button> -->
+
+    <div > 
+      <div :class="TransactionClass"  v-show="isShow" :style="{color : isTransactionSuccess ? 'white' : 'black'}" >
+        <!-- {{ isTransactionSuccess ? 'Transaksi Sukses' : 'Transaksi Gagal' }} -->
+        <p v-if="isTransactionSuccess">
+        Transaksi Sukses
+        </p>
+
+        <p v-else> Transaksi gagal</p>
+      </div>
+
+      <button v-on:click="isTransactionSuccess = !isTransactionSuccess">Submit Transaksi</button>
+      <button v-on:click="isShow = !isShow">Show / Hide</button>
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+  .alert{
+    padding: 1rem;
+    width: 100%;
+    background-color: gray;
+  }
+
+  .alert-success{
+    background-color: green;
+  }
+
+  .alert-danger{
+    background-color: red;
+  }
 </style>
